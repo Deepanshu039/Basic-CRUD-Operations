1a7294830f24273bd9778c6076787e7407763f46
unknown <deepanshu.17220@knit.ac.in>
2023-03-21 01:38:56 +0530

    remove unused var

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
-import Books from './Books';
 
 function Update() {
 
